9157acc0455f3056ac7091ef568c3aaada32903f
Matheusmarazzi <71531067+Matheusmarazzi@users.noreply.github.com>
2023-03-24 14:53:31 -0300

    Add files via upload

---
+++ filas multiplas/app.js
@@ -0,0 +1,6 @@
+
+var processo = [];
+processo[0] = {T_entrada: '0', T_execucao:'5', prioridade:'0'};
+processo[1] = {T_entrada: '8', T_execucao:'9', prioridade:'1'};
+processo[2] = {T_entrada: '5', T_execucao:'2', prioridade:'1'};
+processo[3] = {T_entrada: '9', T_execucao:'1', prioridade:'1'};
