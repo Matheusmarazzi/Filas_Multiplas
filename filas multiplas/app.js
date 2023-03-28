cbf74b81c5e6e2179c9e4068239fb53c74d1f9e3
Matheus de Azevedo Marazzi <71531067+Matheusmarazzi@users.noreply.github.com>
2023-03-28 13:05:36 -0300

    funcao verificar

--- filas multiplas/app.js
+++ filas multiplas/app.js
@@ -1,89 +1,89 @@
 
 var processo = [];
-processo[0] = {nome: 'a', T_entrada: '0', T_execucao:'7', prioridade:'1'};
+processo[0] = {nome: 'a', T_entrada: '0', T_execucao:'5', prioridade:'1'};
 processo[1] = {nome: 'b',T_entrada: '2', T_execucao:'5', prioridade:'0'};
 processo[2] = {nome: 'c',T_entrada: '5', T_execucao:'6', prioridade:'0'};
 processo[3] = {nome: 'd',T_entrada: '6', T_execucao:'9', prioridade:'1'};
 
 
 fatia = 2;
-var espera = [];
+var fila = [];
 
 var tem_max=25;
 var tem_atual= 0;
 //------------------------------------------declaração de variaveis-------------------------
 
-while(tem_max > tem_atual){//inicio comparando os dias
+while(tem_max > tem_atual){
     for(i=0; i<processo.length;i++){
-        if(tem_atual >= processo[i].T_entrada && processo[i].prioridade == 0 && processo[i].T_execucao > 0){// verificando se a prioridade é 0
-            for(j=0;j<processo[i].T_execucao;j++){
-                    $("#execucao").append("<p>"+processo[i].nome+"</p>");
+        if(tem_atual == processo[i].T_entrada){
+            if(processo[i].prioridade == 0){
+                for(j=0; j<processo[i].T_execucao ; j++){
+                    $("#tempo").append("<ul class='tem'>"+tem_atual+"</ul>");
+                    $("#execucao").append("<ul class='exe'>"+processo[i].nome+"</ul>");
                     tem_atual++;
-                                    
-                }
-                processo[i].T_execucao = 0;
-            }else if(tem_atual >= processo[i].T_entrada && processo[i].prioridade == 1 ){//verificando se a prioridade é 1
+                    verificar();
+                }processo[i].T_execucao = 0; 
+            }else{
                 
-                    for(j=0;j<fatia;j++){
-                        if(processo[i].T_execucao >= 0){
-                            $("#execucao").append("<p>"+processo[i].nome+"</p>");
-                            processo[i].T_execucao--;
-                            
-                            tem_atual++;
-                        }
-                }
-                
-
-
             }
+        }        
     }
-} 
+    tem_atual++;
+
+}
+
+
+function verificar(){
+    for (i=0; i < processo.length ; i++) {
+        if (tem_atual == processo[i].T_entrada){
+            fila.push(processo[i]);
+            console.log(fila);
+
+        }
+    }
+    
+}
         
         
         
-        
-        
-//         }else{
-//                     for(j=0;j<fatia;j++){
-//                     $("#execucao").append("<p>"+processo[i].nome+"</p>");
-//                     processo[i].T_execucao--;
-//                     tem_atual++;
-//                     }
-//                     espera.push({id:i});
+
+
+
+
+
+
+// ------------------------------------------------round robin
+// function rodar(){
+//     while(tem_max > tem_atual){//inicio comparando os tempos
+//         for(i=0; i<processo.length;i++){
+//             if(tem_atual >= processo[i].T_entrada && processo[i].prioridade == 0 && processo[i].T_execucao > 0){// verificando se a prioridade é 0
+//                    listarAltaprio();
+//                 }else if(tem_atual >= processo[i].T_entrada && processo[i].prioridade == 1 ){//verificando se a prioridade é 1
+//                     listarBaixaprio();
 //                 }
-            
-
 //         }
-
-
-
-
 //     }
-// }
-
-// if(tem_atual >= processo[i].T_entrada && processo[i].prioridade == 1 ){
-//     if(espera.length >= 1){
-//         let indice = espera[0].id;
-//         while(processo[i].T_execucao > 0){
-//         for(j=0;j<fatia;j++){
-//             $("#execucao").append("<p>"+processo[indice].nome+"</p>");
-//             processo[indice].T_execucao--;
-//             tem_atual++;
+// // } 
+//         function listarAltaprio(){
+//             for(j=0;j<processo[i].T_execucao;j++){
+//                 $("#tempo").append("<ul class='tem'>"+tem_atual+"</ul>");
+//                 $("#execucao").append("<ul class='exe'>"+processo[i].nome+"</ul>");
+//                 tem_atual++;
+                                
+//             } processo[i].T_execucao = 0;
 //         }
-//             espera.shift();
-//             if(processo[indice].T_execucao >0){
-//                 espera.push({id:indice});
-//             }
+//         function listarBaixaprio(){
+//             for(j=0;j<fatia;j++){
+//                 if(processo[i].T_execucao > 0){
+//                     $("#tempo").append("<ul class='tem'>"+tem_atual+"</ul>");
+//                     $("#execucao").append("<ul class='exe'>"+processo[i].nome+"</ul>");
+//                     processo[i].T_execucao--;
+                    
+                    
+//                     tem_atual++;
+//                 }
 //         }
-
-
-
-
-
-
-
-
-
+//     }
 
 
 
